4e7f72cd292e055b961c69e283ff409d2dcbadda
revers3ntropy <josephcoppin@gmail.com>
2022-05-01 13:17:01 +0100

    build script

---
+++ build.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env zx
+
+import { $ } from "zx";
+import now from "performance-now";
+import chalk from 'chalk';
+import * as fs from "fs";
+
+async function upload () {
+	const start = now();
+
+	const paths = fs.readdirSync('./dist/');
+
+	for (const path of paths) {
+		console.log('Uploading path ' + path);
+		if (fs.statSync('./dist/' + path).isDirectory()) {
+			await $`sshpass -f './sshPass.txt' scp -r ./dist/${path} josephcoppin@josephcoppin.com:~/public_html/quiz`;
+			continue;
+		}
+		await $`sshpass -f './sshPass.txt' scp ./dist/${path} josephcoppin@josephcoppin.com:~/public_html/quiz`;
+	}
+
+	console.log(chalk.green('Finished Uploading in ' + (now() - start).toFixed(3)));
+}
+
+async function main () {
+	await upload();
+}
+
+main();
